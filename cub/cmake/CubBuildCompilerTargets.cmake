d3bbfbefb0f0ba228ddb0903b14e1590e74a8f7b
Michael Schellenberger Costa <miscco@nvidia.com>
2023-08-30 08:12:52 +0200

    [skip-tests] Only add the `-use-local-env` flag once for cub

--- cub/cmake/CubBuildCompilerTargets.cmake
+++ cub/cmake/CubBuildCompilerTargets.cmake
@@ -13,7 +13,6 @@ function(cub_build_compiler_targets)
 
   if ("MSVC" STREQUAL "${CMAKE_CXX_COMPILER_ID}")
     list(APPEND cxx_compile_definitions _ENABLE_EXTENDED_ALIGNED_STORAGE)
-    list(APPEND cuda_compile_options "-use-local-env")
 
     append_option_if_available("/W4" cxx_compile_options)
 
